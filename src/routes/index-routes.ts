b03ad783d49963186a9253517f6648c035e3a5b3
lvcaspacifico <lucasreinaldopacificoh@gmail.com>
2025-10-15 20:59:08 -0300

    feat: add rotas de Labels

--- src/routes/index-routes.ts
+++ src/routes/index-routes.ts
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { userRoutes } from "./user-routes";
 import { sessionRoutes } from "./session-routes";
 import { projectRoutes } from "./project-routes";
+import { labelRoutes } from "./label-routes";
 
 const routes = Router();
 
 routes.use("/users", userRoutes);
 routes.use("/sessions", sessionRoutes);
 routes.use("/projects", projectRoutes);
+routes.use("/labels", labelRoutes);
 
 export { routes }
